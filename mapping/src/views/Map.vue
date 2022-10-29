f5b0d6deaada0fd1e7517b7f77fc207f6876ea8f
sunbingqing <sunbingqing@xiaomai5.com>
2022-10-29 17:52:38 +0800

    地图显示

--- mapping/src/views/Map.vue
+++ mapping/src/views/Map.vue
@@ -5,10 +5,11 @@
 <script lang="ts">
 import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
+import MapboxLanguage from '@mapbox/mapbox-gl-language';
 
 export default {
   mounted() {
-    this.init()
+    this.init();
   },
 
   data() {},
@@ -17,19 +18,79 @@ export default {
       // TO MAKE THE MAP APPEAR YOU MUST
       // ADD YOUR ACCESS TOKEN FROM
       // https://account.mapbox.com
-      mapboxgl.accessToken = 'pk.eyJ1Ijoic3VuYmluZ3FpbmciLCJhIjoiY2w5Z3R2a2g2MDgzdDN1cDIxYWUwMjUzcSJ9.JWn1byZ7mTIBqVNZzpEwRA';
+      mapboxgl.accessToken =
+        "pk.eyJ1Ijoic3VuYmluZ3FpbmciLCJhIjoiY2w5Z3R2a2g2MDgzdDN1cDIxYWUwMjUzcSJ9.JWn1byZ7mTIBqVNZzpEwRA";
       const map = new mapboxgl.Map({
         container: "map", // container ID
-        style: "mapbox://styles/mapbox/streets-v11", // style URL
-        center: [-74.5, 40], // starting position [lng, lat]
-        zoom: 9, // starting zoom
-        projection: "globe", // display the map as a 3D globe
+        style: "mapbox://styles/mapbox/outdoors-v11", // style URL
+        center: [120.00, 30.41], // starting position [lng, lat]
+        zoom: 15, // starting zoom
       });
+			console.log('init')
       map.on("style.load", () => {
-        map.setFog({}); // Set the default atmosphere style
+				map.addControl(new MapboxLanguage({
+					defaultLanguage: 'zh-Hans'
+				}));
+        map.addSource("route", {
+          type: "geojson",
+          data: {
+            type: "Feature",
+            properties: {},
+            geometry: {
+              type: "LineString",
+              coordinates: [
+                [-122.483696, 37.833818],
+                [-122.483482, 37.833174],
+                [-122.483396, 37.8327],
+                [-122.483568, 37.832056],
+                [-122.48404, 37.831141],
+                [-122.48404, 37.830497],
+                [-122.483482, 37.82992],
+                [-122.483568, 37.829548],
+                [-122.48507, 37.829446],
+                [-122.4861, 37.828802],
+                [-122.486958, 37.82931],
+                [-122.487001, 37.830802],
+                [-122.487516, 37.831683],
+                [-122.488031, 37.832158],
+                [-122.488889, 37.832971],
+                [-122.489876, 37.832632],
+                [-122.490434, 37.832937],
+                [-122.49125, 37.832429],
+                [-122.491636, 37.832564],
+                [-122.492237, 37.833378],
+                [-122.493782, 37.833683],
+              ],
+            },
+          },
+        });
+        map.addLayer({
+          id: "route",
+          type: "line",
+          source: "route",
+          layout: {
+            "line-join": "round",
+            "line-cap": "round",
+          },
+          paint: {
+            "line-color": "#888",
+            "line-width": 8,
+          },
+        });
       });
     },
   },
 };
 </script>
+<style lang="css" scoped>
+#map {
+  position: absolute;
+  top: 30px;
+  bottom: 0;
+  width: 1300px;
+	height: 660px;
+  margin: 0;
+  padding: 0;
+}
+</style>
 
